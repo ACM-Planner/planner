bed1fb0bb6c83ac554e6626ea22d5cb351b63a81
negebauer <negebauer@uc.cl>
2016-08-19 16:41:57 -0300

    Collaborators list placeholder ft @gantunesr

--- src/components/Collaborators/index.js
+++ src/components/Collaborators/index.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
+import Footer from '../Footer/';
 
 import './style.css';
 
 export class Collaborators extends Component {
+  constructor() {
+    super();
+    this.state = {
+      collaborators: [
+        {
+          name: "Nicolás Gebauer",
+          github: "negebauer",
+        },
+        {
+          name: "Gustavo Antunes",
+          github: "gantunesr",
+        },
+      ]
+    };
+  }
+
   render() {
     return (
       <div className={classnames('Collaborators', this.props.className)} style={this.props.style}>
         <h1 className="Collaborators-title">
-          Collaborators
+          Colaboradores
         </h1>
+        <div className="Collaborators-content">
+        {this.state.collaborators.map((collaborator) => (
+          <div>
+            <a href={`http://github.com/${collaborator.github}`}>{collaborator.name}</a>
+          </div>
+        ))}
+        </div>
+        <Footer />
       </div>
     );
   }
