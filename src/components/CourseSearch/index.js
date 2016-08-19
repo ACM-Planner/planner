de44a9e037d1573a82a54f98a8eb5f2e0f4d623a
Andres Matte <matte.andres@gmail.com>
2016-08-19 11:43:45 -0300

    Added autocomplete

--- src/components/CourseSearch/index.js
+++ src/components/CourseSearch/index.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
+import AutoComplete from 'material-ui/AutoComplete';
 
 import './style.css';
 
 export class CourseSearch extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataSource: [],
+    };
+  }
+  
+  handleUpdateInput = (value) => {
+    this.setState({
+      dataSource: [
+        value,
+        value + value 
+      ],
+    });
+  };
+    
   render() {
     return (
       <div className={classnames('CourseSearch', this.props.className)} style={this.props.style}>
-        <code>src/components/CourseSearch/index.js</code>
+        
+        <AutoComplete
+          hintText="Ingresa tu curso"
+          dataSource={this.state.dataSource}
+          onUpdateInput={this.handleUpdateInput}
+          fullWidth={true}
+          />
+          
       </div>
     );
   }
