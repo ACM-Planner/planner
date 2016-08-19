6c826045ba1cc7d937a24e34b7c429aeb16a2b6a
Patricio López Juri <lopezjuripatricio@gmail.com>
2016-08-19 14:48:59 -0400

    Feature collapse and footer  (#2)
    * Collapsable CurriculumPanel ft @dacasas
    
    * Icon for collapse button in CurriculumPanel ft @dacasas
    
    * CurriculumPanel title ft @dacasas
    
    * Pointer on hover collapse button ft @dacasas
    
    * Placeholder for footer @dacasas
    
    * CurriculumPanel padding top
    
    * Footer ft @dacasas @gantunesr

--- src/components/CurriculumPanel/index.js
+++ src/components/CurriculumPanel/index.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
-import FlatButton from 'material-ui/FlatButton';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
+import Collapse from 'react-collapse';
+import {Icon} from 'react-fa';
 
 import Curriculum from '../Curriculum/';
 
 import './style.css';
 
 export class CurriculumPanel extends Component {
+  constructor() {
+    super();
+    this.state = {
+      collapsed: false,
+    };
+  }
+
+  toggleCollapse = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
   render() {
     return (
       <div className={classnames('CurriculumPanel', this.props.className)} style={this.props.style}>
-        <code className="CurriculumPanel-text">src/components/CurriculumPanel/index.js</code>
-        <div className="CurriculumPanel-content">
-          <Curriculum />
+        <div className="CurriculumPanel-text">
+          Malla curricular
         </div>
-        <div className="CurriculumPanel-button CurriculumPanel-text">
-          <FlatButton label="Primary" primary={true} />
+        <Collapse isOpened={!this.state.collapsed}>
+          <div className="CurriculumPanel-content">
+            <Curriculum />
+          </div>
+        </Collapse>
+        <div className="CurriculumPanel-button" onClick={this.toggleCollapse}>
+          <Icon size="2x" name={`angle-double-${this.state.collapsed ? 'down' : 'up'}`} />
         </div>
       </div>
     );
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
+import {Icon} from 'react-fa';
 
 import './style.css';
 
@@ -8,7 +9,23 @@ export class Footer extends Component {
   render() {
     return (
       <footer className={classnames('Footer', this.props.className)} style={this.props.style}>
-        <code>src/components/Footer/index.js</code>
+        {/* <code>src/components/Footer/index.js</code> */}
+        <div className="Footer-box">
+          <div>
+            DCC working day and night for UC
+          </div>
+        </div>
+        <div className="Footer-box">
+        <div>
+          <a className="Footer-link" href='/posts/'><Icon size="1x" name="heart" />  Colaboradores</a>
+        </div>
+        <div>
+          <a className="Footer-link" href='/posts/'><Icon size="1x" name="github" />  Proyecto</a>
+        </div>
+        <div>
+          <a className="Footer-link" href='/posts/'>Capítulo estudiantil ACM</a>
+        </div>
+        </div>
       </footer>
     );
   }
