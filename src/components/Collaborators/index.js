f4123083c74fa750f6b42c16b8942e9d99fe9db0
Gustavo Antunes <gantunes@uc.cl>
2016-08-19 17:03:01 -0300

    Collaborators data

--- src/components/Collaborators/index.js
+++ src/components/Collaborators/index.js
@@ -11,12 +11,107 @@ export class Collaborators extends Component {
     this.state = {
       collaborators: [
         {
+          name: "CIWS",
+          github: "",
+          url: "http://ciws.cl",
+        }
+        {
           name: "Nicolás Gebauer",
           github: "negebauer",
+          url: "http://negebauer.github.io/",
         },
         {
           name: "Gustavo Antunes",
           github: "gantunesr",
+          url: "http://thisisguto.com",
+        },
+        {
+          name: "Andrés Matte",
+          github: "aamatte",
+          url: "",
+        },
+        {
+          name: "Anders Skog",
+          github: "andersskog",
+          url: "",
+        },
+        {
+          name: "Andres Espinosa",
+          github: "andresespinosapc",
+          url: "",
+        },
+        {
+          name: "",
+          github: "Faturan",
+          url: "",
+        },
+        {
+          name: "Fernando Florenzano",
+          github: "fdflorenzano",
+          url: "",
+        },
+        {
+          name: "Guillermo Figueroa",
+          github: "gafigueroa",
+          url: "",
+        },
+        {
+          name: "Geraldine Monsalve",
+          github: "gnmonsalve",
+          url: "",
+        },
+        {
+          name: "Matías Junemann",
+          github: "mjjunemann",
+          url: "",
+        },
+        {
+          name: "Jaime Castro",
+          github: "jcastro1",
+          url: "",
+        },
+        {
+          name: "Jurgen Heysen",
+          github: "jheysen",
+          url: "",
+        },
+        {
+          name: "Loreto Prieto",
+          github: "LorePrieto",
+          url: "",
+        },
+        {
+          name: "María Fernanda",
+          github: "mf222",
+          url: "",
+        },
+        {
+          name: "",
+          github: "mofadic",
+          url: "",
+        },
+        {
+          name: "Patricio López Juri",
+          github: "mrpatiwi",
+          url: "",
+        },
+        {
+          name: "Oscar Estay",
+          github: "oaestay",
+          url: "",
+        },
+        {
+          name: "Rodrigo Goméz",
+          github: "rkoco",
+          url: "",
+        },
+        {
+          name: "Tamara Covacevich",
+          github: "tamycova",
+        },
+        {
+          name: "Tomas",
+          github: "tegaete",
         },
       ]
     };
