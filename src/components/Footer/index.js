a9400238b76abfce9095199e3a7c04ff3e330bc6
negebauer <negebauer@uc.cl>
2016-08-19 14:24:29 -0300

    Placeholder for footer @dacasas

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
+import {Icon} from 'react-fa';
 
 import './style.css';
 
@@ -8,7 +9,23 @@ export class Footer extends Component {
   render() {
     return (
       <footer className={classnames('Footer', this.props.className)} style={this.props.style}>
-        <code>src/components/Footer/index.js</code>
+        {/* <code>src/components/Footer/index.js</code> */}
+        <div className="Footer-box">
+          <div>
+          Hecho con mucho amor carino y dedicacion por
+          </div>
+        </div>
+        <div className="Footer-box">
+        <div>
+          Link a colaboradores
+        </div>
+        <div>
+          Link al github <Icon size="1x" name="github" />
+        </div>
+        <div>
+          Link al capitulo
+        </div>
+        </div>
       </footer>
     );
   }
